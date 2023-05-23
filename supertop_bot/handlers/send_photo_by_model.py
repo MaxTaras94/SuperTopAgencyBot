7a36e5d3732d915230714d1b69043fd19f410923
Maks Tarasutin <tarasutin@neomaind.com>
2023-05-23 10:58:06 +0300

    Правки, добавил несколько шаблонов, добавил обработчик id и номеров моделей

--- supertop_bot/handlers/send_photo_by_model.py
+++ supertop_bot/handlers/send_photo_by_model.py
@@ -23,11 +23,15 @@ async def choose_params_id_or_phone(update: Update, context: ContextTypes.DEFAUL
 async def send_photos_of_models_potrfolio_for_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Функция отправляет портфолио модели в чат пользователю по переданному id модели"""
     id_model = update.message.text
-    caption_text, link_potrfolio = googlesheetapi.get_link_to_portfolio_of_model(id_model=id_model)
+    markup = ReplyKeyboardMarkup(keyboard_manager_menu, one_time_keyboard=False, resize_keyboard=True)
+    try:
+        caption_text, link_potrfolio = googlesheetapi.get_link_to_portfolio_of_model(id_model=id_model)
+    except:
+        await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=f"Модели с ID: <b>{id_model} </b> нет в БД - попробуйте снова)", reply_markup=markup, parse_mode='HTML')
+        return ConversationHandler.END
     print(caption_text, link_potrfolio)
     links_photo = generate_links_for_sharing(googlesheetapi.get_models_photo(link_potrfolio))
     media_group = []
-    markup = ReplyKeyboardMarkup(keyboard_manager_menu, one_time_keyboard=False, resize_keyboard=True)
     for photo in links_photo:
         media_group.append(InputMediaPhoto(media=photo))
     await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=caption_text, reply_markup =markup)
@@ -39,11 +43,14 @@ async def send_photos_of_models_potrfolio_for_phone(update: Update, context: Con
     """Функция отправляет портфолио модели в чат пользователю по переданному номеру телефона модели"""
     phone_number = update.message.text
     caption_text, link_potrfolio = googlesheetapi.get_link_to_portfolio_of_model(phone_number=phone_number)
+    if len(link_potrfolio) == 0:
+        await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=f"У модели с номером телефона: <b>{phone_number}</b> в БД нет ссылки на портфолио - попробуйте снова)", reply_markup=markup, parse_mode='HTML')
+        return ConversationHandler.END
     links_photo = generate_links_for_sharing(googlesheetapi.get_models_photo(link_potrfolio))
     media_group = []
     markup = ReplyKeyboardMarkup(keyboard_manager_menu, one_time_keyboard=False, resize_keyboard=True)
     for photo in links_photo:
         media_group.append(InputMediaPhoto(media=photo))
-    await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=caption_text, reply_markup =markup)
+    await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=caption_text, reply_markup=markup)
     await context.bot.send_media_group(chat_id=cast(Chat, update.effective_chat).id, media=media_group, protect_content=False)
     return ConversationHandler.END
